757ea55de68ff9bf3c9a59826981ee6813fca4e9
yherreraSMN <yherrera@smn.com.mx>
2022-07-26 16:58:03 -0500

    avance de objetos hasta cap 67

---
+++ Leccion07/08-01-objects.js
@@ -0,0 +1,135 @@
+//* -----------------60. Objetos en JavaScript --------------------------------------------
+let x = 10;
+
+console.log(x.length);
+
+let persona = {
+    nombre: 'Juan',                 //
+    apellido: 'Perez',              // Propiedades del objeto
+    email: 'ejemplo@correo.com',    //
+    edad:  28                       //
+}
+
+persona.nombre
+console.log(persona.nombre);
+console.log(persona.apellido);
+console.log(persona.correo);
+console.log(persona.edad);
+console.log(persona);
+
+
+//* ----------------- 62. Agregar métodos a Objetos en JavaScript --------------------------------------------
+
+persona = {
+    nombre: 'Juan',                 //
+    apellido: 'Perez',              // Propiedades del objeto
+    email: 'ejemplo@correo.com',    //
+    edad:  28  ,                    //
+    nombreCompleto: function(){     // Propiedad con referencia a una función
+        return this.nombre+ " " +this.apellido;
+    }
+}
+console.log(persona);
+
+console.log(persona.nombreCompleto());
+
+
+// *----------------- 64. Agregar métodos a Objetos en JavaScript --------------------------------------------
+
+/*
+    * Forma más común de definir un objeto.
+*/
+
+persona = {
+    nombre: 'Juan',                 //
+    apellido: 'Perez',              // Propiedades del objeto
+    email: 'ejemplo@correo.com',    //
+    edad:  28  ,                    //
+    nombreCompleto: function(){     // Propiedad con referencia a una función
+        return this.nombre+ " " +this.apellido;
+    }
+}
+/*
+    * fin Forma más común de definir un objeto.
+*/
+
+//* forma con new
+
+let persona2 = new Object();
+persona2.nombre = 'Carlos';
+persona2.direccion = 'Chabacano 8';
+persona2.tel = 5596875646;
+
+
+console.log(persona2.nombre);
+console.log(persona2.direccion);
+
+//* ----------------- 65. Acceder a las propiedades de los Objetos en JavaScript --------------------------------------------
+
+persona = {
+    nombre: 'Juan',                 //
+    apellido: 'Perez',              // Propiedades del objeto
+    email: 'ejemplo@correo.com',    //
+    edad:  28  ,                    //
+    nombreCompleto: function(){     // Propiedad con referencia a una función
+        return this.nombre+ " " +this.apellido;
+    }
+}
+
+// forma más básica.
+console.log(persona.nombre);
+
+// forma de acceso similar a un array
+console.log(persona["nombre"]); //objeto['propiedad']  (propiedad = String) 
+
+for( propiedad in persona ){
+    console.log(propiedad);
+    console.log(persona[propiedad]);
+
+}
+
+
+//* ----------------- 66. Agregar/eliminar propiedades al Objetos en JavaScript --------------------------------------------
+
+persona = {
+    nombre: 'Juan',                 //
+    apellido: 'Perez',              // Propiedades del objeto
+    email: 'ejemplo@correo.com',    //
+    edad:  28  ,                    //
+    nombreCompleto: function(){     // Propiedad con referencia a una función
+        return this.nombre+ " " +this.apellido;
+    }
+}
+
+// Agregar   
+persona.tel = 7293369509; // objeto.nombre_propiedad = valor
+
+console.log(persona);
+
+//eliminar
+
+delete persona.tel;
+
+console.log(persona);
+
+//* ----------------- 67. formas de imprimir Objetos en JavaScript --------------------------------------------
+
+// concatenar el valor de cada propiedad
+console.log( persona.nombre + ' ' + persona.apellido );
+
+// for in
+
+for( propiedad in persona ){
+    console.log(persona[propiedad]);
+}
+
+//mediante object.values
+
+let personaArray = Object.values( persona );
+console.log( personaArray );
+
+// mediante json
+
+let personaString = JSON.stringify( persona );
+
+console.log( personaString );
